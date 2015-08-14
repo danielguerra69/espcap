68ad47884f9c694e3e23df65526adac0c05bd1f1
Vic Hargrave <vichargrave@gmail.com>
2015-08-13 22:34:19 -0700

    Loads application configs and builds packet layer objects.

--- espcap_utils.py
+++ espcap_utils.py
@@ -10,25 +10,28 @@ def get_highest_protocol(packet):
     global app_protocols
     if npackets == 3:
         return packet.layers[2].layer_name
-    if npackets == 4:
-        print packet.layers[3].layer_name, packet.layers[3].layer_name in app_protocols
-        if packet.layers[3].layer_name in app_protocols:
-            return packet.layers[3].layer_name
-        return packet.layers[2].layer_name in app_protocols
-    if npackets == 5:
-        if packet.layers[4].layer_name in app_protocols:
-            return packet.layers[4].layer_name in app_protocols
-        return packet.layers[3].layer_name in app_protocols
-    if npackets == 6:
-        return packet.layers[4].layer_name in app_protocols
+    for i in range(3,npackets):
+        if packet.layers[i].layer_name in app_protocols:
+            return packet.layers[i].layer_name
+    return "wtf"
 
 # Returns a dictionary containing the packet layer data
 def get_layers(packet, highest_protocol):
+    npackets = len(packet.layers)
     layers = {}
-    for j in range(len(packet.layers)):
-        layers[packet.layers[j].layer_name] = packet.layers[j]._all_fields
-        if j > 0:
-            layers[packet.layers[j].layer_name]["envelope"] = packet.layers[j-1].layer_name
+    layers["link"] = packet.layers[0]._all_fields
+    layers["network"] = packet.layers[1]._all_fields
+    layers["transport"] = packet.layers[2]._all_fields
+    for j in range(3,npackets):
+        if packet.layers[j].layer_name == highest_protocol:
+            layers["application"] = packet.layers[j]._all_fields
+        else:
+            layers[packet.layers[j].layer_name] = packet.layers[j]._all_fields
+            if j == 3:
+                layers[packet.layers[j].layer_name]["envelope"] = packet.transport_layer.lower()
+            elif j == 4 or j == 5:
+                layers[packet.layers[j].layer_name]["envelope"] = highest_protocol
+
     return layers
 
 # Returns list of network interfaces (nic)
@@ -40,17 +43,19 @@ def list_interfaces():
         interface = interfaces[i].strip(str(i+1)+".")
         print interface
 
-# Get tshark path
+# Get tshark path and supported application protocols
 def load_config():
     global tshark
     config = ConfigParser.ConfigParser()
-    config.readfp(open("./espcap.conf"))
+    config.readfp(open("conf/espcap.conf"))
     path = config.get("paths", "tshark")
     tshark = path+"/tshark"
 
     global app_protocols
-    fp = open("./protocols.list")
-    protcols = fp.readlines()
-    for protocol in protcols:
+    fp = open("conf/protocols.list")
+    protocols = fp.readlines()
+    for protocol in protocols:
+        protocol = protocol.strip()
         app_protocols[protocol] = 1
+
 
