40f1d987f53a2514ea3c8cb2f92be5ed8474a266
Vic Hargrave <vichargrave@gmail.com>
2015-08-14 19:00:26 -0700

    Explain how to use script.

--- conf/protocols.sh
+++ conf/protocols.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# Use this script to generate a new set of tshark protocols.
+# After running, it remove the protocols from protocols.txt
+# by either commenting them out (with # character like a bash
+# script) that you aren't interested then save the new list as
+# file as protocols.list. Note you should always exclude the
+# protocols in the current protocols.ini, should check which
+# ones those are before running this script. 
+
 tshark -G protocols | awk 'BEGIN { FS = "\t" } ; { print $3 }' | sort > protocols.txt
