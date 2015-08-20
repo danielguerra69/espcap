d449322532fe4847811717b143946f6c4d23d79d
Vic Hargrave <vichargrave@gmail.com>
2015-08-19 18:18:46 -0700

    Handle protocols in between link and network, e.g. vpn

--- espcap_utils.py
+++ espcap_utils.py
@@ -4,36 +4,45 @@ supported_protocols = {}
 
 # Get application level protocol
 def get_highest_protocol(packet):
-    npackets = len(packet.layers)
     global supported_protocols
-    if npackets == 2:
-        return packet.layers[1].layer_name
-    elif npackets == 3:
-        return packet.layers[2].layer_name
-    for i in range(3,npackets):
-        if packet.layers[i].layer_name in supported_protocols:
-            return packet.layers[i].layer_name
-    return packet.layers[2].layer_name
+    for layer in reversed(packet.layers):
+        if layer.layer_name in supported_protocols:
+            return layer.layer_name
+    return "wtf"
 
 # Returns a dictionary containing the packet layer data
 def get_layers(packet):
-    npackets = len(packet.layers)
+    n = len(packet.layers)
     highest_protocol = get_highest_protocol(packet)
     layers = {}
+
     layers["link"] = packet.layers[0]._all_fields
-    layers["network"] = packet.layers[1]._all_fields
-    if npackets == 3:
-        layers["transport"] = packet.layers[2]._all_fields
-    for j in range(3,npackets):
-        layers["transport"] = packet.layers[2]._all_fields
+    layer_above_transport = 0
+    for i in range(1,n):
+        if packet.layers[i].layer_name == "arp":
+            layers["network"] = packet.layers[i]._all_fields
+            return highest_protocol, layers
+
+        elif packet.layers[i].layer_name == "ip":
+            layers["network"] = packet.layers[i]._all_fields
+
+        elif packet.layers[i].layer_name == "tcp" or packet.layers[i].layer_name == "udp":
+            layers["transport"] = packet.layers[i]._all_fields
+            if highest_protocol == "tcp" or highest_protocol == "udp":
+                return highest_protocol, layers
+            layer_above_transport = i+1
+            break
+        else:
+            layers[packet.layers[i].layer_name] = packet.layers[i]._all_fields
+            layers[packet.layers[i].layer_name]["envelope"] = packet.layers[i-1].layer_name
+
+    for j in range(layer_above_transport,n):
         if packet.layers[j].layer_name == highest_protocol:
             layers["application"] = packet.layers[j]._all_fields
         else:
             layers[packet.layers[j].layer_name] = packet.layers[j]._all_fields
-            if j == 3:
-                layers[packet.layers[j].layer_name]["envelope"] = packet.transport_layer.lower()
-            elif j == 4 or j == 5:
-                layers[packet.layers[j].layer_name]["envelope"] = highest_protocol
+            layers[packet.layers[j].layer_name]["envelope"] = packet.layers[j-1].layer_name
+
     return highest_protocol, layers
 
 # Returns list of network interfaces (nic)
