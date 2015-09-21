598a85452fe87c56d88a9a52a405eeb76b410246
Vic Hargrave <vichargrave@gmail.com>
2015-09-21 14:09:05 -0700

    Fixed clumsy wording and added usage warning.

--- scripts/protocols.sh
+++ scripts/protocols.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Use this script to generate a new set of tshark protocols. After 
-# running, it remove the protocols from protocols.txt by either 
-# commenting them out (with # character like a bash script) that 
-# you aren't interested then save the new list as the file as 
-# protocols.list. Note you should always exclude the protocols in 
-# the current protocols.ini, should check which ones those are 
-# before running this script. 
+# NOTE: YOU NORMALLY DO NOT NEED TO RUN THIS SCRIPT.  DO SO ONLY AFTER READING BELOW.
+
+# Use this script to generate a new set of tshark protocols. After running it remove 
+# the protocols from the protocols.txt file the script produces by either commenting 
+# out (with # character like a bash script) the protocols you aren't interested in 
+# or just taking htem out of the list compltetely. Then replace conf/protocols.ini 
+# save with your new list. Note you should always exclude the protocols that are 
+# commented out in the current protocols.ini. 
+
 
 tshark -G protocols | awk 'BEGIN { FS = "\t" } ; { print $3 }' | sort > protocols.txt
